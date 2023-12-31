b0d5ebd33c0b7167615b8743b9661c3f73fc0494
lihuigang <lihuigang@meituan.com>
2023-12-31 17:10:30 +0800

    增加 bitmap和array转换的udf 和测试类

---
+++ src/main/java/com/hive/bitmap/udf/BitmapFromArrayUDF.java
@@ -0,0 +1,69 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.hive.bitmap.udf;
+
+import com.hive.bitmap.common.BitmapUtil;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.BinaryObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.roaringbitmap.longlong.Roaring64Bitmap;
+
+import java.io.IOException;
+import java.util.List;
+
+@Description(name = "bitmap_from_array")
+public class BitmapFromArrayUDF extends GenericUDF {
+
+    private transient ListObjectInspector inputOI;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+
+        ObjectInspector input = arguments[0];
+        if (!(input instanceof ListObjectInspector)) {
+            throw new UDFArgumentException("first argument must be a array");
+        }
+
+        this.inputOI = (ListObjectInspector) input;
+
+        return PrimitiveObjectInspectorFactory.javaByteArrayObjectInspector;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[]  args) throws HiveException {
+
+        List<?> list = this.inputOI.getList(args[0].get());
+        Roaring64Bitmap bitmap = new Roaring64Bitmap();
+        list.forEach(e -> bitmap.add(Long.parseLong(e.toString())));
+        try {
+            return BitmapUtil.serializeToBytes(bitmap);
+        } catch (IOException ioException) {
+            throw new HiveException(ioException);
+        }
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return "Usage: bitmap_to_array(bitmap)";
+    }
+}
---
+++ src/main/java/com/hive/bitmap/udf/BitmapToArrayUDF.java
@@ -0,0 +1,80 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.hive.bitmap.udf;
+
+import com.hive.bitmap.common.BitmapUtil;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.BinaryObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.roaringbitmap.longlong.Roaring64Bitmap;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Description(name = "bitmap_to_array")
+public class BitmapToArrayUDF extends GenericUDF {
+
+    private transient BinaryObjectInspector inputOI;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+
+        ObjectInspector input = arguments[0];
+        if (!(input instanceof BinaryObjectInspector)) {
+            throw new UDFArgumentException("first argument must be a binary");
+        }
+
+        this.inputOI = (BinaryObjectInspector) input;
+
+        return ObjectInspectorFactory.getStandardListObjectInspector(
+                PrimitiveObjectInspectorFactory.javaLongObjectInspector);
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[]  args) throws HiveException {
+        if (args[0] == null) {
+            return 0;
+        }
+        byte[] inputBytes = this.inputOI.getPrimitiveJavaObject(args[0].get());
+
+        try {
+            Roaring64Bitmap bitmapValue = BitmapUtil.deserializeToBitmap(inputBytes);
+            List<Long> expList = new ArrayList<>();
+            Iterator<Long> iterator = bitmapValue.iterator();
+            while (iterator.hasNext()) {
+                expList.add(iterator.next());
+            }
+            return expList;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            throw new HiveException(ioException);
+        }
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return "Usage: bitmap_to_array(bitmap)";
+    }
+}
